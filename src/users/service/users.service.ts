0edfbd8d2c95560a859ef219bb207a752457d73d
An70 <antonionavapaz@outlook.com>
2022-06-05 17:11:18 -0400

    Solved the error for ConfigModule import An error about the ConfigModule was resolved in the testing module by adding:       imports: [ConfigModule.forRoot({ envFilePath: '.env' })], on the     const module: TestingModule = await Test.createTestingModule({         imports: [ConfigModule.forRoot({ envFilePath: '.env' })],         controllers: [UsersController],         providers: [UsersService],         }).compile(); while creating the module, so having some hours solving this, should be documented

--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
+  constructor(private configService: ConfigService) {}
+
   getAllUsers() {
-    return `ALL users listed`;
+    return `ALL users listed ${this.configService.get('DATABASE')}`;
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './service/users.service';
 
 @Module({
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
   controllers: [UsersController],
   providers: [UsersService],
 })
